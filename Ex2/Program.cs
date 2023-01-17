fb8a05230f8a11247b928f296cdbcad5d0c464b1
Сергей <basov888@ya.ru>
2023-01-17 12:37:03 +0700

    New project Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача №3. Напишите программу, которая будет выдавать
+название дня недели по заданному номеру.
+3 -> Среда  5 -> Пятница */
+
+Console.Write("Введите номер дня недели: ");
+string Day = Console.ReadLine ();
+if (Day == "1")
+{
+    Console.WriteLine("Понедельник");
+} 
+if (Day == "2")
+{
+    Console.WriteLine("Вторник");
+}  
+if (Day == "3")
+{
+    Console.WriteLine("Среда");
+}  
+if (Day == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if (Day == "5")
+{
+    Console.WriteLine("Пятница");
+}    
+if (Day == "6")
+{
+    Console.WriteLine("Суббота");
+}  
+if (Day == "7")
+{
+    Console.WriteLine("Воскресенье");
+}  
