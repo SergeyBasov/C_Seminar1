d820927194b1c0d81814cba4cc0d0e53fa0c0951
Сергей <basov888@ya.ru>
2023-01-17 12:40:43 +0700

    New project Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int result = num % 10;
+Console.WriteLine(result);
+
+/*
+var str = Console.ReadLine();
+Console.WriteLine("Код последнего символа: " + num[num.Length - 1]);
+*/
