74014c12870f1224feebbe02e8004418d52a6a00
Сергей <basov888@ya.ru>
2023-01-17 12:33:02 +0700

    создана новая программа Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача №1. Напишите программу, которая на вход принимает два числа и проверяет,
+является ли первое число квадратом второго. 
+a = 25, b = 5 -> да  a = 2, b = 10 -> нет  a = 9, b = -3 -> да  
+a = -3 b = 9 -> нет */
+ 
+Console.Write("Введите число 1: ");
+int numberА = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberА == numberB * numberB) 
+{
+Console.WriteLine("Первое число ЯВЛЯЕТСЯ квадратом второго числа");
+ }
+else
+{
+    Console.WriteLine("Первое число НЕ ЯВЛЯЕТСЯ квадратом второго числа");
+}
